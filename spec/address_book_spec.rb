cf62c799c04bbab6418ba52145a3e9443ebe6c06
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-01-12 12:56:58 -0600

    Assignment 5 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe AddressBook do
      end
   end
 
+  describe "#remove_entry" do
+       it "removes only one entry to the address book" do
+         book = AddressBook.new
+         book.add_entry('Alyssa Broussard', '010.320.4321', 'alyssa.broussard@broussard.com')
+
+         name = "Ada Lovelace"
+         phone_number = "010.012.1815"
+         email = "augusta.king@lovelace.com"
+         book.add_entry(name, phone_number, email)
+
+         expect(book.entries.size).to eq(2)
+         book.remove_entry(name, phone_number, email)
+         expect(book.entries.size).to eq(1)
+         expect(book.entries.first.name).to eq("Alyssa Broussard")
+       end
+     end
+
   describe "#add_entry" do
        it "adds only one entry to the address book" do
          book = AddressBook.new
